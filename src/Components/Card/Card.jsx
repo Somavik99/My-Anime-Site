33a85a4f27ca05fc73fa1cd51a6c2850f5e1b36e
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-13 22:10:33 +0530

    Card.css

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -3,16 +3,18 @@ import React from "react";
 import { Card } from "@chakra-ui/react";
 const CardPage = ({ image, title_english, title_japanese }) => {
   return (
-    <Card className="Card">
-      <p className="Card_image">
-        <img src={image} />
-      </p>
+    <div className="CardPage">
+      <Card className="Card">
+        <p className="Card_image">
+          <img src={image} />
+        </p>
 
-      <p style={{ color: "whitesmoke" }}>
-        <span sty>Title: {title_english}</span>
-        <span className="jap_title">{title_japanese}</span>
-      </p>
-    </Card>
+        <p style={{ color: "black", overflow: "hidden" }}>
+          <span sty>Title: {title_english}</span>
+          <span className="jap_title">{title_japanese}</span>
+        </p>
+      </Card>
+    </div>
   );
 };
 
