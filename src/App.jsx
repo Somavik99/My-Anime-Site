edeef97d06821484a73b20d95e0505951eeabade
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-13 13:57:09 +0530

    Api Fetch in Axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import HomePage from "./Components/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <HomePage/>
     </div>
   );
 }
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
-}
+  const [HomePageState, setHomePageState] = useState([]);
+  const HomePageApi = async () => {
+    await axios
+      .get(
+        "https://api.jikan.moe/v4/anime?q=One Piece, Draon Ball Super, Kimetsu No Yaiba, Bleach, Attack On Titan&sfw&limit=90"
+      )
+      .then((resp) => {
+        console.log(resp.data);
+        setHomePageState(resp.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return <div></div>;
+};
 
-export default HomePage
+export default HomePage;
