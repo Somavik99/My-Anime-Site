25a8c4c4fb47cc4be33820d5ca23e4091fcf2dd1
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-13 17:24:00 +0530

    Css Implied

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -1,10 +1,17 @@
 import "./Card.css";
 import React from "react";
 import { Card } from "@chakra-ui/react";
-const CardPage = ({ image }) => {
+const CardPage = ({ image, title_english, title_japanese }) => {
   return (
     <Card className="Card">
-     <img src={image}/>
+      <p className="Card_image">
+        <img src={image} />
+      </p>
+
+      <p style={{ color: "whitesmoke" }}>
+        <span sty>Title: {title_english}</span>
+        <span className="jap_title">{title_japanese}</span>
+      </p>
     </Card>
   );
 };
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,14 +1,12 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import CardPage from "../Card/Card";
-
+import "./HomePage.css";
 const HomePage = () => {
   const [HomePageState, setHomePageState] = useState([]);
   const HomePageApi = async () => {
     await axios
-      .get(
-        "https://api.jikan.moe/v4/anime?q=One Piece, Draon Ball Super, Kimetsu No Yaiba, Bleach, Attack On Titan&sfw&limit=90"
-      )
+      .get("https://api.jikan.moe/v4/anime?latest")
       .then((resp) => {
         console.log(resp.data.data);
         return setHomePageState(resp.data.data);
@@ -23,9 +21,17 @@ const HomePage = () => {
   }, []);
 
   return (
-    <div>
+    <div className="HomePage">
       {HomePageState.map((item, index) => {
-        return <CardPage key={index} image={item.images.webp.image_url} />;
+        return (
+          <div key={index} className="Homepage_Card">
+            <CardPage
+              image={item.images.jpg.image_url}
+              title_english={item.title_english}
+              title_japanese={item.title_japanese}
+            />
+          </div>
+        );
       })}
     </div>
   );
