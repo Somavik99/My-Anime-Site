7b1e0d8d925e074a30da33a5eb2032b717e69354
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-16 11:24:12 +0530

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,9 @@ import HomePage from "./Components/HomePage/HomePage";
 function App() {
   return (
     <div className="App">
+      <h1 style={{fontSize:"70px",fontWeight:"bolder"}} >Anime.to</h1>
       <HomePage />
+
     </div>
   );
 }
