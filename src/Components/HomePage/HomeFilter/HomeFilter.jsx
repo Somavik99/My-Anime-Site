a36a9c53df33dfce227f6f586c478b0fe344ccd1
Somavik99 <94855290+Somavik99@users.noreply.github.com>
2023-03-21 17:14:01 +0530

    Props and hooks are applied

--- src/Components/HomePage/HomeFilter/HomeFilter.jsx
+++ src/Components/HomePage/HomeFilter/HomeFilter.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import "./HomeFilter.css";
 
 const HomeFilter = ({ HomePageState }) => {
-  return <div className="HomeFilter">
-HomePage
-  </div>;
+  return (
+    <div className="HomeFilter">
+      <h1>Popular</h1>
+      <h1>Top rated</h1>
+    </div>
+  );
 };
 
 export default HomeFilter;
